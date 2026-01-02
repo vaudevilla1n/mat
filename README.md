278f3f22bca49ed9c071495d742317e7cc977f03
Jake Faard <jakefaard@gmail.com>
2026-01-02 04:19:34 -0500

    minor changes v2

--- README.md
+++ README.md
@@ -7,12 +7,12 @@ grammar
 statement	::= simple_statment | compound_statement
 
 simple_statement	::= assignment | return | print ';'
-compound_statement	::= function | condition
+compound_statement	::= function | if
 
 function	::= fn identifier '(' param (',' param)* ')' body
 param		::= identifier
 
-condition	::= 'if' expression body ( 'else' 'if' expression body )* [ 'else' body ]
+if			::= 'if' expression body ( 'else' 'if' expression body )* [ 'else' body ]
 
 body		::= '{' (simple_statement | condition)+ '}'
 
@@ -37,16 +37,19 @@ love you so much
 
 ast
 ```
-stmt	::= if(expr *bool, stmt true[], stmt false[])
-|	func(id name, stmt body[])
+stmt	::= if(expr *bool, stmt body[], stmt else_body[])
+|	func(id name, id args[], stmt body[])
 |	assign(id var, expr value)
+|	return(expr value)
+|	print(expr value)
 |	expression(expr val)
 
 expr	::= binary	(expr left, binaryop op, expr right) 
 |	unary(unaryop op, expr right)
-|	group(expr val)
+|	group(expr)
 |	call(id name, expr args[])
-|	number(f64 number)
+|	number(f64)
+|	bool(bool)
 |	var(id name)
 
 unaryop		::= ( 'return' | 'print' | '+' | '-')
--- src/parser.rs
+++ src/parser.rs
@@ -3,21 +3,25 @@ use crate::lexer::*;
 
 
 #[derive(Debug)]
-pub enum Expr<'s> {
-	Binary(Rc<Expr<'s>>, Token, Rc<Expr<'s>>),
-	Unary(Token, Rc<Expr<'s>>),
-	Group(Rc<Expr<'s>>),
-	Call(String, &'s[Rc<Expr<'s>>]),
+pub enum Expr {
+	Binary(Rc<Expr>, Token, Rc<Expr>),
+	Unary(Token, Rc<Expr>),
+	Group(Rc<Expr>),
+	Call(String, Vec<Expr>),
 	Number(f64),
+	Bool(bool),
 	Var(String),
 }
 
 #[derive(Debug)]
-pub enum Stmt<'s> {
-	If(Expr<'s>, &'s[Rc<Stmt<'s>>], &'s[Rc<Stmt<'s>>]),
-	Func(String, &'s[Rc<Stmt<'s>>]),
-	Assign(String, Expr<'s>),
-	Expression(Expr<'s>),
+pub enum Stmt {
+	Body(Vec<Stmt>),
+	If(Expr, Rc<Stmt>, Option<Rc<Stmt>>),
+	Func(String, Vec<String>, Rc<Stmt>),
+	Assign(String, Expr),
+	Print(Expr),
+	Return(Expr),
+	Expression(Expr),
 }
 
 #[derive(Debug)]
@@ -46,51 +50,84 @@ impl<'s> Parser<'s> {
 		Parser{tokens: l}
 	}
 
-	fn err(&self, msg: String) -> Error {
+	fn err(&self, msg: &str) -> Error {
 		let (line, col) = self.tokens.pos();
 
-		Error{line: line, col: col, msg: msg}
+		Error{line: line, col: col, msg: msg.to_string()}
 	}
 
-	fn expect(&mut self, expected: Token, msg: String) -> Result<(), Error> {
+	fn check(&mut self, expected: Token) -> bool {
 		if self.tokens.peek() == expected {
+			true
+		} else {
+			false
+		}
+	}
+
+	fn consumed(&mut self, expected: Token) -> bool {
+		if self.check(expected) {
 			self.tokens.next();
 
+			true
+		} else {
+			false
+		}
+	}
+
+	fn expect(&mut self, expected: Token, msg: &str) -> Result<(), Error> {
+		if self.consumed(expected) {
 			Ok(())
 		} else {
 			Err(self.err(msg))
 		}
 	}
 
-	fn call(&mut self) -> Result<Expr<'s>, Error> {
-		todo!();
+	fn call(&mut self, func_name: String) -> Result<Expr, Error> {
+		let mut args: Vec<Expr> = Vec::new();
+		if self.tokens.peek() != Token::RParen {
+			let arg = self.expr()?;
+			args.push(arg);
+
+			while self.consumed(Token::Comma) {
+				let arg = self.expr()?;
+				args.push(arg);
+			}
+
+		}
+
+		self.expect(Token::RParen, "missing closing parentheses")?;
+
+		Ok(Expr::Call(func_name, args))
 	}
 
-	fn literal(&mut self) -> Result<Expr<'s>, Error> {
+	fn literal(&mut self) -> Result<Expr, Error> {
 		match self.tokens.next() {
 			Token::Number(x) => Ok(Expr::Number(x)),
+
+			Token::True => Ok(Expr::Bool(true)),
+			Token::False => Ok(Expr::Bool(false)),
 
 			Token::LParen => {
 				let e = self.expr()?;
 
-				self.expect(Token::RParen, "expected closing parentheses".to_string())?;
+				self.expect(Token::RParen, "expected closing parentheses")?;
 
 				Ok(Expr::Group(Rc::new(e)))
 			}
 
 			Token::Identifier(name) => {
-				if self.tokens.peek() == Token::LParen {
-					self.call()
+				if self.consumed(Token::LParen) {
+					self.call(name)
 				} else {
 					Ok(Expr::Var(name))
 				}
 			}
 
-			tok => Err(self.err(format!("expected literal got {tok:?}"))),
+			tok => Err(self.err(&format!("expected literal got {tok:?}"))),
 		}
 	}
 
-	fn unary(&mut self) -> Result<Expr<'s>, Error> {
+	fn unary(&mut self) -> Result<Expr, Error> {
 		if let Token::Plus | Token::Minus = self.tokens.peek() {
 			let op = self.tokens.next();
 			let right = self.literal()?;
@@ -101,7 +138,7 @@ impl<'s> Parser<'s> {
 		}
 	}
 
-	fn exponent(&mut self) -> Result<Expr<'s>, Error> {
+	fn exponent(&mut self) -> Result<Expr, Error> {
 		let left = self.unary()?;
 
 		if let Token::Caret = self.tokens.peek() {
@@ -114,7 +151,7 @@ impl<'s> Parser<'s> {
 		}
 	}
 
-	fn factor(&mut self) -> Result<Expr<'s>, Error> {
+	fn factor(&mut self) -> Result<Expr, Error> {
 		let mut e = self.exponent()?;
 
 		while let Token::Star | Token::Slash = self.tokens.peek() {
@@ -127,7 +164,7 @@ impl<'s> Parser<'s> {
 		Ok(e)
 	}
 
-	fn term(&mut self) -> Result<Expr<'s>, Error> {
+	fn term(&mut self) -> Result<Expr, Error> {
 		let mut e = self.factor()?;
 
 		while let Token::Plus | Token::Minus = self.tokens.peek() {
@@ -140,7 +177,7 @@ impl<'s> Parser<'s> {
 		Ok(e)
 	}
 
-	fn comparison(&mut self) -> Result<Expr<'s>, Error> {
+	fn comparison(&mut self) -> Result<Expr, Error> {
 		let mut e = self.term()?;
 
 		while let Token::Equal | Token::Greater | Token::GreaterEqual | Token::Less | Token::LessEqual = self.tokens.peek() {
@@ -153,47 +190,154 @@ impl<'s> Parser<'s> {
 		Ok(e)
 	}
 
-	fn expr(&mut self) -> Result<Expr<'s>, Error> {
+	fn expr(&mut self) -> Result<Expr, Error> {
 		self.comparison()
 	}
 
-	fn expression(&mut self) -> Result<Stmt<'s>, Error> {
+	fn expression(&mut self) -> Result<Stmt, Error> {
 		let expr = self.expr()?;
 
-		self.expect(Token::Semicolon, "missing semicolon".to_string())?;
+		self.expect(Token::Semicolon, "missing semicolon")?;
 
 		Ok(Stmt::Expression(expr))
 	}
 
-	fn assign(&mut self) -> Result<Stmt<'s>, Error> {
-		todo!();
+	fn assign(&mut self, var_name: String) -> Result<Stmt, Error> {
+		let expr = self.expr()?;
+
+		self.expect(Token::Semicolon, "missing semicolon")?;
+
+		Ok(Stmt::Assign(var_name, expr))
 	}
 
-	pub fn parse(&mut self) -> Result<Vec<Stmt<'s>>, Error> {
-		let mut stmts: Vec<Stmt<'s>> = Vec::new();
+	fn return_stmt(&mut self) -> Result<Stmt, Error> {
+		let expr = self.expr()?;
+		self.expect(Token::Semicolon, "missing semicolon")?;
+
+		Ok(Stmt::Return(expr))
+	}
+
+	fn print_stmt(&mut self) -> Result<Stmt, Error> {
+		let expr = self.expr()?;
+		self.expect(Token::Semicolon, "missing semicolon")?;
+
+		Ok(Stmt::Print(expr))
+	}
+
+	fn body(&mut self) -> Result<Stmt, Error> {
+		self.expect(Token::LBracket, "missing opening brace for if statement")?;
+
+		let mut body: Vec<Stmt> = Vec::new();
+		while !self.check(Token::RBracket) && !self.check(Token::EOF) {
+			let stmt = self.statement()?;
+			match stmt {
+				Stmt::Func(_, _, _) => { return Err(self.err("function definitions musn't be nested")); }
+
+				_ => { body.push(stmt); }
+			};
+		}
+
+		self.expect(Token::RBracket, "missing closing brace")?;
+
+		Ok(Stmt::Body(body))
+	}
+
+	fn if_stmt(&mut self) -> Result<Stmt, Error> {
+		let condition = self.expr()?;
+		let body = self.body()?;
+		let else_body = if self.consumed(Token::Else) {
+			if self.consumed(Token::If) {
+				Some(Rc::new(self.if_stmt()?))
+			} else {
+				Some(Rc::new(self.body()?))
+			}
+		} else {
+			None
+		};
+
+		Ok(Stmt::If(condition, Rc::new(body), else_body))
+	}
+
+	fn func(&mut self) -> Result<Stmt, Error> {
+		let func_name = match self.tokens.next() {
+			Token::Identifier(name) => name,
+
+			_ => { return Err(self.err("need function name in declaration")) }
+		};
+
+		self.expect(Token::LParen, "expected opening parentheses after function name")?;
+
+		let mut args: Vec<String> = Vec::new();
+		if let Token::Identifier(arg) = self.tokens.peek() {
+			self.tokens.next();
+			args.push(arg);
+
+			while self.consumed(Token::Comma) {
+				let arg = match self.tokens.next() {
+					Token::Identifier(var) => var,
+
+					_ => { return Err(self.err("expected variable in function definition")); }
+				};
+
+				args.push(arg);
+			}
+		}
+
+		self.expect(Token::RParen, "expected closing parentheses")?;
+
+		let body = self.body()?;
+
+		Ok(Stmt::Func(func_name, args, Rc::new(body)))
+	}
+
+	fn statement(&mut self) -> Result<Stmt, Error> {
+		match self.tokens.peek() {
+			Token::Number(_) | Token::LParen => {
+				self.expression()
+			}
+
+			Token::Identifier(name) => {
+				self.tokens.next();
+
+				if self.consumed(Token::Assign) {
+					self.assign(name)
+				} else {
+					self.expression()
+				}
+			}
+
+			Token::If => {
+				self.tokens.next();
+				self.if_stmt()
+			}
+
+			Token::Return => {
+				self.tokens.next();
+				self.return_stmt()
+			}
+
+			Token::Print => {
+				self.tokens.next();
+				self.print_stmt()
+			}
+
+			Token::Func => {
+				self.tokens.next();
+
+				self.func()
+			}
+
+			tok => {
+				Err(self.err(&format!("unexpected token {tok:?}")))
+			}
+		}
+	}
+
+	pub fn parse(&mut self) -> Result<Vec<Stmt>, Error> {
+		let mut stmts: Vec<Stmt> = Vec::new();
 
 		while self.tokens.peek() != Token::EOF {
-			let stmt = match self.tokens.peek() {
-				Token::Number(_) | Token::LParen => {
-					self.expression()?
-				}
-
-				Token::Identifier(_) => {
-					let mut tokens = self.tokens.clone();
-					tokens.next();
-
-					if tokens.peek() == Token::Assign {
-						self.assign()?
-					} else {
-						self.expression()?
-					}
-				}
-
-				tok => {
-					return Err(self.err(format!("unexpected token {tok:?}")))
-				}
-			};
-
+			let stmt = self.statement()?;
 			stmts.push(stmt);
 		}
 
